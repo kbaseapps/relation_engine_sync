d6e3cfd326d5f8856d822b89e5eb9a1a252007d4
Jay R Bolton <jayrbolton@gmail.com>
2019-07-02 11:24:43 -0700

    Add basic event handler logic

--- src/kafka_consumer.py
+++ src/kafka_consumer.py
@@ -2,6 +2,7 @@
 Consume workspace update events from kafka.
 """
 import json
+import traceback
 from confluent_kafka import Consumer, KafkaError
 from .utils.config import get_config
 
@@ -36,9 +37,45 @@ def run():
             continue
         val = msg.value().decode('utf-8')
         try:
-            data = json.loads(val)
-            print('received', data)
+            msg = json.loads(val)
+            _handle_msg(msg)
         except ValueError as err:
             print(f'JSON parsing error: {err}')
             print(f'Message content: {val}')
     consumer.close()
+
+
+def _handle_msg(msg):
+    """Receive a kafka message."""
+    event_type = msg.get('evtype')
+    wsid = msg.get('wsid')
+    if not wsid:
+        print(f'Invalid wsid in event: {wsid}')
+        return
+    if not event_type:
+        print(f"Missing 'evtype' in event: {msg}")
+        return
+    print(f'Received {msg["evtype"]} for {wsid}/{msg.get("objid", "?")}')
+    try:
+        if event_type in ['IMPORT', 'NEW_VERSION', 'COPY_OBJECT', 'RENAME_OBJECT']:
+            print('run importer')
+        elif event_type == 'IMPORT_NONEXISTENT':
+            print('run importer if nonexistent')
+        elif event_type == 'OBJECT_DELETE_STATE_CHANGE':
+            print('delete obj')
+        elif event_type == 'WORKSPACE_DELETE_STATE_CHANGE':
+            print('delete obj for all in workspace')
+        elif event_type in ['CLONE_WORKSPACE', 'IMPORT_WORKSPACE']:
+            print('import whole workspace')
+        elif event_type == 'SET_GLOBAL_PERMISSION':
+            print('set global permission for a workspace')
+        else:
+            print(f"Unrecognized event {event_type}.")
+            return
+    except Exception as err:
+        print('=' * 80)
+        print(f"Error indexing:\n{type(err)} - {err}")
+        print(msg)
+        print(err)
+        traceback.print_exc()
+        print('=' * 80)
