2754583cd9819dc695f0d7cacf140c00b76505a8
Jay R Bolton <jayrbolton@gmail.com>
2019-07-08 10:21:29 -0700

    Add a script in the test runner to wait for RE API to be healthy

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,7 +4,9 @@
 flake8 /kb/module/src
 mypy --ignore-missing-imports /kb/module/src
 bandit -r /kb/module/src
-# Run the server and fork
+# Run the app and fork
 sh /kb/module/scripts/entrypoint.sh &
+# Wait for the RE API to be healthy
+python /kb/module/scripts/wait_for_api.py
 # Run the tests
 python -m unittest discover /kb/module/src/test/
